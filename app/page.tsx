08cb61a20aa3187ce52475e91ed5591b2a0a8f41
Azeez Ariyibi <Olabisi14>
2023-11-10 00:23:22 +0100

    Added Alt attribute to next Image component

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+         <AcmeLogo /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -38,10 +38,10 @@ export default function Page() {
       src="/hero-mobile.png"
       width ={560}
       height = {620}
-      className = "block md:hidden"/>
+      className = "block md:hidden"
+      alt='Screenshots of the dashboard project showing desktop and mobile versions'
+      />
         </div>
-      }
-      }
       </div>
     </main>
   );
